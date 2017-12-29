21c079e983bb17efd4f031b6c310999096f4ca14
Luke Nezda <lnezda@gmail.com>
2017-12-29 12:12:36 -0600

    set elasticsearch_version metadata before potentially upgrading

--- upgrade-to-5.6.sh
+++ upgrade-to-5.6.sh
@@ -3,9 +3,13 @@
 set -e
 
 # Example script to restart an elasticsearch node
+ES_VERSION=5.6.4
 echo "Running updates on $NODE"
 echo "Host: $HOST"
 echo "Port: $PORT"
+echo "ES_VERSION: $ES_VERSION"
+
+gcloud compute instances add-metadata es-firetruck-3 --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
 
 ansible-playbook -l $HOST ../../es.yml
 

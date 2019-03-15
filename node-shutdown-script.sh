701807f20ceb4f6d4471bfbb7563126a8ad23973
Luke Nezda <lnezda@gmail.com>
2019-03-15 15:20:10 -0500

    gssh not working - used this most recently as myself on ansible-new

--- node-shutdown-script.sh
+++ node-shutdown-script.sh
@@ -10,7 +10,8 @@ echo "Port: $PORT"
 shopt -s expand_aliases
 alias gssh='gcloud compute ssh --zone us-central1-b'
 
-gssh $HOST --ssh-flag "-o LogLevel=quiet" --command "sudo service elasticsearch stop"
+#gssh $HOST --ssh-flag "-o LogLevel=quiet" --command "sudo service elasticsearch stop"
+ssh $HOST -o LogLevel=quiet "sudo service elasticsearch stop"
 
 # must exit cleanly
 exit 0

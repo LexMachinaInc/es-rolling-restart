8a46f653c5126d292d97089688566aa7c10a6a51
Luke Nezda <lnezda@gmail.com>
2019-06-11 10:22:49 -0500

    update for extra es nodes (live in es-firetruck and es-yolo)

--- upgrade-to-6.sh
+++ upgrade-to-6.sh
@@ -10,10 +10,11 @@ echo "Port: $PORT"
 echo "ES_VERSION: $ES_VERSION"
 
 gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
+gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "num_nodes"="6"
 
 ansible-playbook -l $HOST ../../es.yml
 
-ssh $HOST -o LogLevel=quiet "sudo apt-get update && sudo apt-get -y upgrade && sudo reboot"
+#ssh $HOST -o LogLevel=quiet "sudo apt-get update && sudo apt-get -y upgrade && sudo reboot"
 
 # Those commands should include starting the elasticsearch 
 # instance once updates are complete.

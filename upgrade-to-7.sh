b09bebe8519a12b8b027ae5aebfb1bcf0e3e369c
Luke Nezda <lnezda@gmail.com>
2020-07-17 10:48:28 -0500

    upgrade-to-7.sh

--- upgrade-to-7.sh
+++ upgrade-to-7.sh
@@ -3,15 +3,18 @@
 set -e
 
 # Example script to restart an elasticsearch node
-ES_VERSION=5.6.12
+ES_VERSION=7.8.0
 echo "Running updates on $NODE"
 echo "Host: $HOST"
 echo "Port: $PORT"
 echo "ES_VERSION: $ES_VERSION"
 
 gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
+gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "num_nodes"="6"
 
 ansible-playbook -l $HOST ../../es.yml
+
+#ssh $HOST -o LogLevel=quiet "sudo apt-get update && sudo apt-get -y upgrade && sudo reboot"
 
 # Those commands should include starting the elasticsearch 
 # instance once updates are complete.

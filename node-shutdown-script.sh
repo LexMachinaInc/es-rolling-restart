28cea6ea29f7e1007526907617b83d7a95ffb6a3
Luke Nezda <lnezda@gmail.com>
2020-07-17 10:54:30 -0500

    dont care

--- node-shutdown-script.sh
+++ node-shutdown-script.sh
@@ -11,7 +11,7 @@ shopt -s expand_aliases
 alias gssh='gcloud compute ssh --zone us-central1-b'
 
 #gssh $HOST --ssh-flag "-o LogLevel=quiet" --command "sudo service elasticsearch stop"
-ssh $HOST -o LogLevel=quiet "sudo service elasticsearch stop"
+ssh $HOST -o LogLevel=quiet -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no "sudo service elasticsearch stop"
 
 # must exit cleanly
 exit 0

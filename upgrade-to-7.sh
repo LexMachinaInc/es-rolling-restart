f29c09d7e50f23aa3bdafdf785a52df31ce9324c
Luke Nezda <lnezda@gmail.com>
2020-12-03 12:20:27 -0600

    7.8.1 -> 7.10

--- upgrade-to-7.sh
+++ upgrade-to-7.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Example script to restart an elasticsearch node
-ES_VERSION=7.8.1
+ES_VERSION=7.10
 echo "Running updates on $NODE"
 echo "Host: $HOST"
 echo "Port: $PORT"
 echo "ES_VERSION: $ES_VERSION"
 
-#gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
+gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
 #gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "num_nodes"="6"
 
 ansible-playbook -l $HOST ../../es.yml

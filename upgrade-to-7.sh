bab79065c67d8fa9cd609a778ca47c222e09054e
Luke Nezda <lnezda@gmail.com>
2020-12-10 10:23:34 -0600

    update upgrade-to-7.sh since not in production-configurations/vendor/es_rolling_restart anymore - assumes sibline checkout ../production-configurations/

--- upgrade-to-7.sh
+++ upgrade-to-7.sh
@@ -9,14 +9,13 @@ echo "Host: $HOST"
 echo "Port: $PORT"
 echo "ES_VERSION: $ES_VERSION"
 
-gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
-#gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "num_nodes"="6"
+# Those commands should include starting the elasticsearch instance and service once updates are complete.
 
-ansible-playbook -l $HOST ../../es.yml
+gcloud compute instances add-metadata $HOST --zone us-central1-b -q --metadata "elasticsearch_version"="$ES_VERSION"
+
+ansible-playbook -l $HOST ../production-configurations/es.yml
 
 #ssh $HOST -o LogLevel=quiet "sudo apt-get update && sudo apt-get -y upgrade && sudo reboot"
 
-# Those commands should include starting the elasticsearch 
-# instance once updates are complete.
 
 exit 0
